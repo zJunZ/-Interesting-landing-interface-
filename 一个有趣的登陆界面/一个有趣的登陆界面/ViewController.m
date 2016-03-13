d5cd5ea4e92c5a13f60ad2e27a17cbe2956d2792
ZZ <zz.qq.com>
2016-03-14 00:34:24 +0800

    1.0完成✅

--- 一个有趣的登陆界面/一个有趣的登陆界面/ViewController.m
+++ 一个有趣的登陆界面/一个有趣的登陆界面/ViewController.m
@@ -12,9 +12,10 @@
 #define mainSizi [UIScreen mainScreen].bounds.size
 #define rectLogin CGRectMake((mainSizi.width - LoginWidth)/2, 100, LoginWidth, LoginHight)
 #define  rectLeftHand CGRectMake(5, 90, 40, 65)
+#define offset 60
 #define  rectRightHand CGRectMake(LoginWidth -40, 90, 40, 65)
 #define rectLeftGone CGRectMake(mainSizi.width/2 -100, vLogin.frame.origin.y - 22, 44, 44)
-#define rectRightGone CGRectMake(mainSizi.width/2+ 60, vLogin.frame.origin.y - 22, 44, 44)
+#define rectRightGone CGRectMake(mainSizi.width/2+ offset, vLogin.frame.origin.y - 22, 44, 44)
 @interface ViewController ()<UITextFieldDelegate>
 {
     UITextField *textUser;
@@ -64,8 +65,74 @@
     imageRightHandGone = [[UIImageView alloc] initWithFrame:rectRightGone];
     imageRightHandGone.image = [UIImage imageNamed:@"未标题-1"];
     [self.view addSubview:imageRightHandGone];
+    
+    textUser = [[UITextField alloc] initWithFrame:CGRectMake(30, 30, vLogin.frame.size.width - offset, 44)];
+    textUser.delegate = self;
+    textUser.layer.cornerRadius = 5;
+    textUser.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    textUser.layer.borderWidth = 0.5;
+    textUser.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+    textUser.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIImageView *imageUser = [[UIImageView alloc] initWithFrame:CGRectMake(11, 11, 22, 22)];
+    imageUser.image = [UIImage imageNamed:@"iconfont-user"];
+    [textUser.leftView addSubview:imageUser];
+    [vLogin addSubview:textUser];
+    
+    textPwd = [[UITextField alloc] initWithFrame:CGRectMake(30, 90, vLogin.frame.size.width - offset, 44)];
+    textPwd.delegate = self;
+    textPwd.layer.cornerRadius = 5;
+    textPwd.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    textPwd.layer.borderWidth = 0.5;
+    textPwd.leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+    textPwd.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIImageView *imagePwd = [[UIImageView alloc] initWithFrame:CGRectMake(11, 11, 22, 22)];
+    imagePwd.image = [UIImage imageNamed:@"iconfont-password"];
+    [textPwd.leftView addSubview:imagePwd];
+    [vLogin addSubview:textPwd];
 }
+-(void)textFieldDidBeginEditing:(UITextField *)textField{
+    if ([textField isEqual:textUser]) {
+        if (showType != ZzLoginShowType_PASS) {
+            showType =ZzLoginShowType_USER;
+            return;
+        }
+        showType = ZzLoginShowType_USER;
+        [UIView animateWithDuration:0.5 animations:^{
+            imageLeftHand.frame =CGRectMake(imageLeftHand.frame.origin.x - 55, imageLeftHand.frame.origin.y
+                                            + 30, imageLeftHand.frame.size.width, imageLeftHand.frame.size.height);
+            imageRightHand.frame =CGRectMake(imageRightHand.frame.origin.x + 52, imageRightHand.frame.origin.y
+                                            + 30, imageRightHand.frame.size.width, imageRightHand.frame.size.height);
+            
+            imageLeftHandGone.frame =CGRectMake(imageLeftHandGone.frame.origin.x - 70, imageLeftHandGone.frame.origin.y
+                                             , imageLeftHandGone.frame.size.width, imageLeftHandGone.frame.size.height);
+            imageRightHandGone.frame =CGRectMake(imageRightHandGone.frame.origin.x + 30, imageRightHandGone.frame.origin.y
+                                           , imageRightHandGone.frame.size.width, imageRightHandGone.frame.size.height);
+        } completion:^(BOOL finished) {
+        }];
+    }
+    else if ([textField isEqual:textPwd]){
+        if (showType == ZzLoginShowType_PASS) {
+            showType = ZzLoginShowType_PASS;
+            return;
+        }
+        showType =ZzLoginShowType_PASS;
+        [UIView animateWithDuration:0.5 animations:^{
+            imageLeftHand.frame =CGRectMake(imageLeftHand.frame.origin.x + 55, imageLeftHand.frame.origin.y
+                                            - 30, imageLeftHand.frame.size.width, imageLeftHand.frame.size.height);
+            imageRightHand.frame =CGRectMake(imageRightHand.frame.origin.x - 52, imageRightHand.frame.origin.y
+                                             - 30, imageRightHand.frame.size.width, imageRightHand.frame.size.height);
+            
+            imageLeftHandGone.frame =CGRectMake(imageLeftHandGone.frame.origin.x + 70, imageLeftHandGone.frame.origin.y
+                                                , 0,0);
+            imageRightHandGone.frame =CGRectMake(imageRightHandGone.frame.origin.x - 30, imageRightHandGone.frame.origin.y
+                                                 , 0, 0);
+        } completion:^(BOOL finished) {
+        }];
 
+    }
+}
 
 
 @end
