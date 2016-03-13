1f32f3dcb8010aa85ab2fa4e005f381ed9bde200
ZZ <zz.qq.com>
2016-03-13 21:52:20 +0800

    项目初始化

---
+++ 一个有趣的登陆界面/一个有趣的登陆界面/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  一个有趣的登陆界面
+//
+//  Created by Mac on 16/3/13.
+//  Copyright © 2016年 zz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 一个有趣的登陆界面/一个有趣的登陆界面/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  一个有趣的登陆界面
+//
+//  Created by Mac on 16/3/13.
+//  Copyright © 2016年 zz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
